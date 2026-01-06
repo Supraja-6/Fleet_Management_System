b14a65609f41545a0f574d46fbf59e04cbd5ae06
Supraja-6/Start <kotturusupraja2004@gmail.com>
2026-01-06 12:33:03 +0530

    Project start and display message

---
+++ src/Ecoride_main.py
@@ -0,0 +1,7 @@
+class EcoRideMain:
+    def display(self):
+        print("Welcome to Eco-Ride Urban Mobility System")
+
+if __name__ == "__main__":
+    ecoridemain = EcoRideMain
+    ecoridemain.display()
