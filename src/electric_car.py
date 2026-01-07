abe9dc57b8654b7211e6cafe055b14230e798e55
Supraja-6/Start <kotturusupraja2004@gmail.com>
2026-01-07 14:42:56 +0530

    UC4: Implemented Abstraction

--- src/electric_car.py
+++ src/electric_car.py
@@ -4,8 +4,8 @@ class ElectricCar(Vehicle):
         super().__init__(vehicle_id, model, battery_percentage)
         self.seating_capacity = seating_capacity
 
-    """def calculate_trip_cost(self, distance):
-        return 5 + (0.5 * distance)"""
+    def calculate_trip_cost(self, distance):
+        return 5 + (0.5 * distance)
     
     def display(self):
         super().display()
